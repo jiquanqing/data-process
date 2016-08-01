6d2657968c1ca90bcf524774b13c453969ae7733
qingjiquan <qingjiquan@bubugao.com>
2016-08-01 11:35:25 +0800

    加入第二种恢复机制 从mysql中

--- data-process-crawler-download/src/main/java/com/qjq/crawler/download/contier/DownLoadWorkQueueManger.java
+++ data-process-crawler-download/src/main/java/com/qjq/crawler/download/contier/DownLoadWorkQueueManger.java
@@ -6,13 +6,17 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.qjq.crawler.download.dao.mysql.CrawlerJobMapper;
 import com.qjq.crawler.download.dao.redis.RedisStoreManger;
+import com.qjq.crawler.download.domain.CrawlerJob;
 
 @Component
 public class DownLoadWorkQueueManger {
 
     private Map<String, DownLoadWorkQueue> workQueue = new HashMap<String, DownLoadWorkQueue>();
 
+    @Autowired
+    CrawlerJobMapper crawlerJobMapper;
     @Autowired
     RedisStoreManger redisStoreManger;
 
@@ -26,19 +30,31 @@ public class DownLoadWorkQueueManger {
         return downLoadWorkQueue;
     }
 
-    //从redis中恢复内存数据
+    // 从redis中恢复内存数据
     public void recoverWordQueue(String jobId) {
         DownLoadWorkQueue downLoadWorkQueue = new DownLoadWorkQueue(jobId, -1, -1);
         workQueue.put(jobId, downLoadWorkQueue);
-        
+
         Integer tot = Integer.valueOf(redisStoreManger.get(downLoadWorkQueue.getTotKey()));
         Integer down = Integer.valueOf(redisStoreManger.get(downLoadWorkQueue.getDownKey()));
-        
-        
+
         redisStoreManger.hset(downLoadWorkQueue.getTotKey(), tot, downLoadWorkQueue.getCountTimeOut());
         redisStoreManger.hset(downLoadWorkQueue.getDownKey(), down, downLoadWorkQueue.getCountTimeOut());
 
+    }
+
+    // 从mysql中恢复数据 但是此种方式会有问题，因为redis中的数据是延时5s写入数据库的 不到万不得已不采用
+    public void recoverWordQueueByMysql(String jobId) {
+        DownLoadWorkQueue downLoadWorkQueue = new DownLoadWorkQueue(jobId, -1, -1);
+        workQueue.put(jobId, downLoadWorkQueue);
+
+        CrawlerJob crawlerJob = crawlerJobMapper.selectByPrimaryKey(jobId);
+
+        Integer down = crawlerJob.getCrawlednum();
+        Integer tot = down + 1;
         
+        redisStoreManger.hset(downLoadWorkQueue.getTotKey(), tot, downLoadWorkQueue.getCountTimeOut());
+        redisStoreManger.hset(downLoadWorkQueue.getDownKey(), down, downLoadWorkQueue.getCountTimeOut());
     }
 
     public Map<String, DownLoadWorkQueue> getWorkQueue() {
