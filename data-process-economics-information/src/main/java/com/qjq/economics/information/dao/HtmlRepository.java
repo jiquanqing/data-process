7090cfd58dfe6a9643dd21b7cd4425268be84d81
qingjiquan <qingjiquan@bubugao.com>
2016-08-03 11:34:40 +0800

    财经信息parse入库基本流程和框架走通

---
+++ data-process-economics-information/src/main/java/com/qjq/economics/information/dao/HtmlRepository.java
@@ -0,0 +1,46 @@
+package com.qjq.economics.information.dao;
+
+import java.util.List;
+
+import org.data.process.model.HtmlObject;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.stereotype.Repository;
+
+
+
+@Repository
+public class HtmlRepository implements AbstractRepository, InitializingBean {
+
+    @Autowired
+    MongoTemplate mongoTemplate;
+
+    @Value("${scrapy.html.collection:html}")
+    private String collectionName;
+
+    public void insert(HtmlObject htmlObject) {
+
+        mongoTemplate.save(htmlObject, collectionName);
+    }
+
+    public void insert(HtmlObject htmlObject, String collectionName) {
+        mongoTemplate.save(htmlObject, collectionName);
+    }
+
+    public void afterPropertiesSet() throws Exception {
+
+    }
+
+    public HtmlObject findOne(String uid) {
+        return mongoTemplate.findOne(new Query(Criteria.where("uid").is(uid)), HtmlObject.class, collectionName);
+    }
+
+    public List<HtmlObject> find(String query) {
+        return null;
+    }
+
+}
--- data-process-economics-information/src/main/java/com/qjq/economics/information/jms/ParseServiceJmsListener.java
+++ data-process-economics-information/src/main/java/com/qjq/economics/information/jms/ParseServiceJmsListener.java
@@ -1,17 +1,57 @@
 package com.qjq.economics.information.jms;
 
+import org.data.process.model.HtmlObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qjq.economics.information.dao.HtmlRepository;
+import com.qjq.economics.information.domain.ParseConfig;
+import com.qjq.economics.information.domain.ParseInformation;
+import com.qjq.economics.information.service.InformationDataProcessService;
+import com.qjq.parser.extractor.ExtractorPage;
+
+@Service
 public class ParseServiceJmsListener implements JmsListenerStrategy {
+
+    private static Logger logger = LoggerFactory.getLogger(ParseServiceJmsListener.class);
 
     private String jmsQueue = "Parse_Service_Job_Queue";
 
+    @Autowired
+    HtmlRepository htmlRepository;
+    @Autowired
+    InformationDataProcessService dataProcessService;
+
     @Override
     public String getJmsQueue() {
-        return null;
+        return jmsQueue;
     }
 
     @Override
     public void onMessage(String text) {
+        logger.info("接收到parse任务,text={}", text);
 
+        String uid = text;
+        if (uid != null) {
+            HtmlObject htmlObject = htmlRepository.findOne(uid);
+            if (htmlObject != null) {
+                logger.info("开始parse uid={}", uid);
+                ParseConfig config = new ParseConfig();
+                config.setUid(uid);
+                config.setUrl(htmlObject.getUrl());
+                config.setContent(htmlObject.getContent());
+
+                ExtractorPage extractorPage = dataProcessService.parse(config);
+                ParseInformation information = dataProcessService.extractorPageConvert2ParseInformation(extractorPage);
+                dataProcessService.insert(information);
+
+                logger.info("uid={},parse成功已经插入到mysql中", uid);
+            } else {
+                logger.error("Mongo Not Find Uid={}", uid);
+            }
+        }
     }
 
 }
